46a5c5d7d75e2a91b5a4e660c9765c6f575dc8b7
Hridoyshom <hshom827@gmail.com>
2022-05-26 00:53:35 +0600

    question answered 2

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -12,6 +12,13 @@ const Blogs = () => {
                     5. Not to receive unnecessary props.
                 </p>
                 <h1 className='text-2xl ml-8 ' >What are the different ways to manage a state in a React application?</h1>
+                <p>
+                    There are few states that are used to manage states in react application. <br />
+                    1. Communication state: when we try to store information in different state then communication state helps us to do that. The error message about failed to pay or the change of selector,type etc is done by communication state. <br />
+                    2.Data State: Data state temporarily store the information. Like from a project it store project name, contacts details etc. <br />
+                    3.
+
+                </p>
             </div>
         </div>
     );
