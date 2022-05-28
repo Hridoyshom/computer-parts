aaa8865c408029bc76d044dd3f5515a9b771f5a5
Hridoyshom <hshom827@gmail.com>
2022-05-28 23:45:32 +0600

    portfolio edited

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -42,6 +42,21 @@ const Blogs = () => {
 
                 </p>
             </div>
+            <div>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">Hridoy Shom</h2>
+                        <p>My Email: hshom827@gmail.com </p>
+                        <p>Education: B.sc in CSE </p>
+                        <p>Skills as a developer: HTML,JAVASCRIPT,React,Nodejs,MongoDB </p>
+                        <p>project link: https://computer-parts-2a1a0.web.app/</p>
+                        <p><p>project link: https://computer-parts-2a1a0.web.app/</p></p>
+                        <p><p>project link: https://computer-parts-2a1a0.web.app/</p></p>
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
