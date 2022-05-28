0677ab223dd9941c76b9e56a8a7b822b4bf63c85
Hridoyshom <hshom827@gmail.com>
2022-05-28 14:34:28 +0600

    admin made

--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from './Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
 
@@ -12,6 +13,29 @@ const Users = () => {
     return (
         <div>
             <h1>all users: {users.length}</h1>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            users.map(user => <UserRow
+                                key={user._ud}
+                                user={user}
+                            ></UserRow>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
